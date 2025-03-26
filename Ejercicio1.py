7099bd32d45f4c80a370331c3bb375a684994fcd
JorgeCarlos04 <jorgecarlosnunez3@gmail.com>
2025-03-25 21:52:49 -0400

    Update Ejercicio1.py

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.neighbors import KNeighborsClassifier 
 
 # Datos de entrenamiento del Modelo
-MatrizX = np.array([[2, 0],[4, 4],[1, 1],[2, 4],[2, 2],[2, 3],[3, 4],[3, 3]])
+MatrizX = np.array([[2, 12],[4, 9],[10, 1],[9, 4],[2, 6],[2, 18],[3, 4],[3, 3]])
 Clases = np.array([0, 1, 0, 1, 0, 1, 0, 1]) 
 
 # Configurar el clasificador K-NN con k=1
@@ -10,6 +10,6 @@ KNN = KNeighborsClassifier(n_neighbors=1, metric='manhattan')
 KNN.fit(MatrizX, Clases)
 
 # Clasificar el nuevo caso (2.5, 2.5)
-Casos = np.array([[2.5, 2.5]])
+Casos = np.array([[5, 5]])
 C_preajustada_1 = KNN.predict(Casos)
-print(f"Clase preajustada para los valores (2.5, 2.5): {C_preajustada_1[0]}")
+print(f"Clase preajustada para los valores (5, 5): {C_preajustada_1[0]}")
